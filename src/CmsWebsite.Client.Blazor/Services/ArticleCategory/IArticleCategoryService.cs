a0fb71f976ad9f9095ae66937d10908c013264e5
phatht <phatht@vietinfo.tech>
2022-04-19 18:24:03 +0700

    Commit source

--- src/CmsWebsite.Client.Blazor/Services/ArticleCategory/IArticleCategoryService.cs
+++ src/CmsWebsite.Client.Blazor/Services/ArticleCategory/IArticleCategoryService.cs
@@ -5,7 +5,6 @@ namespace CmsWebsite.Client.Blazor.Services.ArticleCategory
     public interface IArticleCategoryService
     {
         Task<ArticleCategoryDTO> GetArticleCategory(long id);
-
         Task<bool> CreateArticleCategory(ArticleCategoryRequest request);
         Task<bool> UpdateArticleCategory(long articleId, ArticleCategoryDTO ac);
 
