0c60d0cfbbf855b834a62a8886032c02138f297b
phamgialam2000 <75935354+phamgialam2000@users.noreply.github.com>
2022-04-25 18:12:42 +0700

    Commit CKEditor

---
+++ src/CmsWebsite.Client.Blazor/Components/EditorComponent.razor.cs
@@ -0,0 +1,49 @@
+﻿
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace CmsWebsite.Client.Blazor.Components
+{
+    partial class EditorComponent : IDisposable
+    {
+        [Inject] public IJSRuntime JSRuntime { get; set; }
+        [Parameter] public string EditorValue { get; set; }
+        [Parameter] public EventCallback<string> EditorValueChanged { get; set; }
+        [Parameter] public string EditorHeight { get; set; }
+        [Parameter] public string EditorWidth { get; set; }
+
+        string _editorId;
+        public string EditorId
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_editorId))
+                    _editorId = $"ckeditor_{Guid.NewGuid().ToString().ToLower().Replace("-", "")}";
+                return _editorId;
+            }
+            set => _editorId = value;
+        }
+
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+
+            if (firstRender)
+                await JSRuntime.InvokeVoidAsync("CreateEditor", EditorId, EditorValue, EditorHeight, DotNetObjectReference.Create(this));
+
+            await base.OnAfterRenderAsync(firstRender);
+        }
+
+
+        [JSInvokable]
+        public async Task OnEditorChanged(string data)
+        {
+            await EditorValueChanged.InvokeAsync(data);
+        }
+
+        public void Dispose()
+        {
+            JSRuntime.InvokeVoidAsync("DestroyEditor", EditorId);
+        }
+    }
+}
---
+++ src/CmsWebsite.Client.Blazor/wwwroot/js/editor.js
@@ -0,0 +1,106 @@
+﻿editors = {};
+
+
+function CreateEditor(editorId, defaultValue, height, dotNetReference) {
+
+	ClassicEditor
+
+		.create(document.getElementById(editorId), {
+
+
+
+			toolbar: {
+
+				items: [
+
+					'heading',
+
+					'|',
+
+					'bold',
+
+					'italic',
+
+					'link',
+
+					'bulletedList',
+
+					'numberedList',
+
+					'|',
+
+					'outdent',
+
+					'indent',
+
+					'|',
+
+					'imageUpload',
+
+					'blockQuote',
+
+					'insertTable',
+
+					'mediaEmbed',
+
+					'undo',
+
+					'redo'
+
+				]
+
+			},
+
+			language: 'en',
+
+			licenseKey: '',
+
+		})
+
+		.then(editor => {
+
+			editors[editorId] = editor;
+
+			editor.setData(defaultValue);
+
+			editor.editing.view.change(writer => {
+
+				writer.setStyle('height', height, editor.editing.view.document.getRoot());
+
+			});
+
+
+
+			editor.model.document.on('change:data', () => {
+
+				let data = editor.getData();
+
+				dotNetReference.invokeMethodAsync('OnEditorChanged', data);
+
+			});
+
+		})
+
+		.catch(error => {
+
+			console.error(error);
+
+		});
+
+
+
+}
+
+
+
+
+
+function DestroyEditor(editorId) {
+
+	editors[editorId].destroy().then(() => delete editors[editorId])
+
+		.catch(error => console.log(error));
+
+
+
+}
