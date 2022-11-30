fe5821887a2d11cd52173e7aa1b2ddbcd11fd17d
tnanhkiet <70634660+tnanhkiet@users.noreply.github.com>
2022-11-30 14:43:23 +0700

    Commit Source

---
+++ src/CmsWebsite.Client.Blazor/wwwroot/js/main.js
@@ -0,0 +1,25 @@
+﻿function loadZaloDSK() {
+    ZaloSocialSDK.reload();
+}
+//---------------------------------------------------------------------------------------
+function loadVideo() {
+    document.getElementById("videoArticle").load();
+}
+//---------------------------------------------------------------------------------------
+function focusSearch() {
+    document.getElementById("txtSearch").focus();
+}
+//---------------------------------------------------------------------------------------
+let scrollButton = document.getElementById("btnScroll");
+window.onscroll = function () { scrollFunction() };
+function scrollFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        scrollButton.style.display = "block";
+    } else {
+        scrollButton.style.display = "none";
+    }
+}
+function onScrollEvent() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
