b1dbc440f25770a15001ad398608031c949e2e77
tnanhkiet <70634660+tnanhkiet@users.noreply.github.com>
2022-12-05 11:37:36 +0700

    Commit Source

---
+++ src/CmsWebsite.Api/Infrastructure/Migrations/20221205042943_addViewArticle.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CmsWebsite.Api.Infrastructure.Migrations
+{
+    public partial class addViewArticle : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "View",
+                table: "CmsArticle",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "View",
+                table: "CmsArticle");
+        }
+    }
+}
